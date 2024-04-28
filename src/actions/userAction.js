1b15459ad70dd4fa247f609fa8b7d7555d57c8c2
Kuldeep Yadav <109398194+Kuldeep12e@users.noreply.github.com>
2024-04-29 05:21:10 +0530

    Update userAction.js

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -108,7 +108,7 @@ export const loadUser = () => async (dispatch) => {
 
         dispatch({ type: LOAD_USER_REQUEST });
 
-        const { data } = await axios.get('https://fashion-fushion-backend.onrender.com//api/v1/me' ,{ withCredentials: true });
+        const { data } = await axios.get('https://fashion-fushion-backend.onrender.com/api/v1/me' ,{ withCredentials: true });
 
         dispatch({
             type: LOAD_USER_SUCCESS,
